80209e25ca00ce08ad0c2ef253311eece8648b92
gov <govi218mu@gmail.com>
2018-08-13 10:59:04 -0400

    create lambda route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const aws = require('./aws.js');
 const uuid = require('uuid');
 const fs = require('fs');
 
-const port = 8888;
+const port = 8888 || process.env.PORT;
 
 let app = express();
 
@@ -25,7 +25,9 @@ app.post('/upload_code', (req, res) => {
         let bucket_name = 'data-x'; // uuid.v4();
         let key_name = 'hello_world.txt';
         
-    /*  // code below creates new bucket
+        /*
+        
+        // code below creates new bucket
         let s3_promise = new aws.S3({apiVersion:'2006-03-01'}).createBucket({Bucket: bucket_name}).promise();
     
         s3_promise
@@ -41,7 +43,9 @@ app.post('/upload_code', (req, res) => {
             })
             .catch((api_err) => {
                 console.log(api_err);
-            }); */
+            }); 
+        
+        */
     
         // Create params for putObject call
         let object_params = {Bucket: bucket_name, Key: key_name, Body: data};
@@ -61,14 +65,42 @@ app.post('/upload_code', (req, res) => {
             });
         });
     })
-    .catch((err) => {
-        // fs error
-        res.send({
-            err: err
+});
+
+app.post('/create_lambda', (req, res) => {
+    fs.readFile('./tst.zip', (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        // should get this from req
+        let name = 'hello_world';
+
+        // create promise object for create lambda api call
+        let lambda = new aws.Lambda().createFunction({
+            Code: {
+                ZipFile: data
+            },
+            FunctionName: name,
+            Role: 'arn:aws:iam::573790977052:role/lambda',
+            Runtime: 'nodejs8.10',
+            Handler: 'hello_world'
+        }).promise();
+
+        // handle promise
+        lambda
+        .then((data) => {
+            res.send({status: 1});
+            console.log(data);
+        })
+        .catch((err) => {
+            res.send({
+                err: err
+            });
+            console.log(err);
         });
     });
 });
-
 
 app.listen(port, () => {
     console.log('Listening on port: ' + port);
